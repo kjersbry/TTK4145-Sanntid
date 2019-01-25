54a311cf06e0398fb2587c09d6935bb282ec20ef
kjersbry <kjersbry@stud.ntnu.no>
2019-01-25 12:47:06 +0100

    working UDP client

--- Exercise3/UDP-client.go
+++ Exercise3/UDP-client.go
@@ -4,19 +4,36 @@ import "net"
 import "fmt"
 import "os"
 
+//SERVER IP: 10.100.23.242
+const SERVER_IP = "10.100.23.242"
+
+func UDPclient_serverdial(ip string, port int){
+	addr := fmt.Sprintf("%s:%d", ip, port)
+	sock, _ := net.Dial("udp", addr)
+	sock.Write([]byte("Helloooo"))
+}
 
 
-func main() {
-
-	listen, err := net.ListenPacket("udp", ":30000")
+func UDPclient_serverlisten(port int){
+	portt := fmt.Sprintf(":%d", port)
+	listen, err := net.ListenPacket("udp", portt)
 
 	if err!= nil {
 		fmt.Println("Error: ", err)
 		os.Exit(0)
 	}
+
 	buffer:= make([]byte, 1024)
 	listen.ReadFrom(buffer)
 
-	fmt.Printf("IP?: %s \n" , buffer)
+	fmt.Printf("IP: %s \n" , buffer)
 	defer listen.Close()
+
+}
+
+func main() {
+
+	UDPclient_serverdial(SERVER_IP, 20006)
+	UDPclient_serverlisten(20006)
+
 }
