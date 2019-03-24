e6fdba626049ad6351df5f89b29e5063ffd2267c
omhaalan <36660834+omhaalan@users.noreply.github.com>
2019-03-24 21:47:55 +0100

    Orderassigner updated

--- ELEVATOR-CODE/orderassigner/orderassigner.go
+++ ELEVATOR-CODE/orderassigner/orderassigner.go
@@ -6,6 +6,7 @@ import (
 	"../states"
 )
 
+//Kjersti's
 func AssignOrder(drv_button <-chan elevio.ButtonEvent, add_order chan<- types.AssignedOrder){
 	for{
 		select{
@@ -18,51 +19,40 @@ func AssignOrder(drv_button <-chan elevio.ButtonEvent, add_order chan<- types.As
 	}	
 }
 
-//This function is responsible for reciving unassigned orders from elevator_io.
-//The order is evaluated by assignAlg and assigned to an elevator.
-//The the order and the ID of the assigned elevator is sent to a channel.
-/*func AssignOrder(drv_button <-chan elevio.ButtonEvent, add_order chan<- types.AssignedOrder){
-	for{
-		select{
-		case order:= <- drv_button:
-			selected_elevator := assignAlg(order)
-			assgined_order := AssignedOrder{order, selected_elevator}
-			add_order <- assigned_order
+
+//Ole's
+func AssignOrder(drv_button <-chan ButtonEvent, add_order chan<- AssignedOrder) {
+	for {
+		select {
+		case order := <-drv_button:
+			workingElevs = states.WorkingElevs 
+			//WorkingElevs returns a slice with all elevators that are connected and operational. Must be made.
+			var selected_elevator = assignAlg(order, workingElevs)
+			assigned_order := AssignedOrder{order, selected_elevator}
+			add_order <- assigned_order //skriver resultat til order
 		}
-	}	
-}*/
+	}
+}
+func assignAlg(new_order ButtonEvent, elevators []Elevator) int {
+	var best_chooice int
+	var best_duration float64
 
-//This function adds the new order to the que of every avaible elevator. (Only done locally)
-//the function timeToIdle is run once per elevator. 
-//It returns the ID of the elevator with the lowest return value from timeToIdle
-/*func assignAlg(new_order) int {
-	var best_choice int 
-	var best_duration int
+	var currentDuration float64
 
-	var currentElevator elevatorstates.Elevator
-	var currentDuration int
+	for _, elev := range elevators {
+		if elev.Operational {
+			elev.Orders[new_order.Floor][new_order.Button].State = OS_AcceptedOrder
+			currentDuration = timeToIdle(elev)
 
-	/*Note the following alternative: As of now, the readElevator function is called once for every elevator. We may be better off
-	by making one function call, e.g. readAllElevators. And then go through the returned array. Such a function would be usefull for 
-	the network mudule as well*/
-/*
-	for _, elevator := range states.WorkingElevators() {               //WorkingElevators must be created
-
-		//add new_order to current elevator   <-The appropriate function should be made and saved in orders.
-												//The sturcture of the order array should be clearly defined before this line is added.
-
-
-		currentElevator = elevatorstates.ReadElevator(/*elevator <- Specifying witch elevator that will be examined*//*)
-		currentDuration = timeToIdle(currentElevator)
-
-		if(currentDuration < best_duration){
-			best_chooice = currentElevator.Elevator_ID 
-		} 
+			if (currentDuration < best_duration) || best_duration == 0 {
+				best_duration = currentDuration
+				best_chooice = elev.Elevator_ID
+			}
+		}
 	}
 
-	return best_choice
-}*/
-/*
+	return best_chooice
+}
 
 //TimeToIdle gives an estimate of how much that time that will elapse before an elevator as handled all its requests.
 func timeToIdle(e Elevator) float64 {
