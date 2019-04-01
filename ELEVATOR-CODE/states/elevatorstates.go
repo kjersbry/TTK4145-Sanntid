7d15b32b5e69ae42865fbf7d15cfc9ab6e9e5cde
kjersbry <kjersbry@stud.ntnu.no>
2019-04-01 13:12:31 +0200

    h

--- ELEVATOR-CODE/states/elevatorstates.go
+++ ELEVATOR-CODE/states/elevatorstates.go
@@ -11,45 +11,46 @@ import (
 	"../types"
 )
 
-var all_elevators map[string]types.Elevator //todo
-var localelev_ID string
+//Only UpdateElevators() is writing to allElevators
+var allElevators map[string]types.Elevator
+var localElevID string
 
 
 func InitElevators(localID string, drvFloors <-chan int) {
-	localelev_ID = localID
-	all_elevators = make(map[string]types.Elevator)
+	localElevID = localID
+	allElevators = make(map[string]types.Elevator)
 
 	//Initialize local elevator
 	var emptyElevator types.Elevator
-	emptyElevator.ElevatorID = localelev_ID
-	all_elevators[localelev_ID] = emptyElevator
+	emptyElevator.ElevatorID = localElevID
+	allElevators[localElevID] = emptyElevator
 
 	var ord [constants.N_FLOORS][constants.N_BUTTONS](types.Order)
-	setFields(types.ES_Idle, -1, elevio.MD_Stop, localelev_ID)
-	setOrderList(ord, localelev_ID)
-	setOperational(true, localelev_ID)
-	setConnected(true, localelev_ID)
-	lamps.SetAllLamps(localelev_ID, all_elevators)
+	setFields(types.ES_Idle, -1, elevio.MD_Stop, localElevID)
+	setOrderList(ord, localElevID)
+	setOperational(true, localElevID)
+	setConnected(true, localElevID)
+	lamps.SetAllLamps(localElevID, allElevators)
 	elevio.SetDoorOpenLamp(false)
 
 	//wait to allow floor signal to arrive if we start on a floor
 	time.Sleep(time.Millisecond * 50)
 	select {
 	case floor := <-drvFloors:
-		setFloor(floor, localelev_ID)
+		setFloor(floor, localElevID)
 		elevio.SetFloorIndicator(floor)
 	default:
 		//init between floors
-		if all_elevators[localelev_ID].Floor == -1 {
-			setDir(elevio.MD_Down, localelev_ID)
-			elevio.SetMotorDirection(all_elevators[localelev_ID].Direction)
-			setState(types.ES_Moving, localelev_ID)
+		if allElevators[localElevID].Floor == -1 {
+			setDir(elevio.MD_Down, localElevID)
+			elevio.SetMotorDirection(allElevators[localElevID].Direction)
+			setState(types.ES_Moving, localElevID)
 		}
 	}
 }
 
 
-/*This is the only routine that is writing to all_elevators*/ //todo remove comment
+/*This is the only routine that is writing to allElevators*/
 func UpdateElevator(
 	/*I/O Channels for handling FSM*/
 	updateState <-chan types.ElevatorState, updateFloor <-chan int, updateDirection <-chan elevio.MotorDirection,
@@ -61,36 +62,36 @@ func UpdateElevator(
 	for {
 		select {
 		case newState := <- updateState:
-			setState(newState, localelev_ID)
+			setState(newState, localElevID)
 
 		case newFloor := <-updateFloor:
-			setFloor(newFloor, localelev_ID)
+			setFloor(newFloor, localElevID)
 			floorReached <- true
 
 		case newDir := <-updateDirection:
-			setDir(newDir, localelev_ID)
+			setDir(newDir, localElevID)
 
 		case buttonPress := <-drvButton:
-			assignedOrder := orders.AssignOrder(all_elevators, localelev_ID, buttonPress)
+			assignedOrder := orders.AssignOrder(allElevators, localElevID, buttonPress)
 			setOrdered(assignedOrder.Order.Floor, int(assignedOrder.Order.Button), assignedOrder.ElevatorID, false)
-			if assignedOrder.ElevatorID == localelev_ID && assignedOrder.Order.Button == elevio.BT_Cab {
+			if assignedOrder.ElevatorID == localElevID && assignedOrder.Order.Button == elevio.BT_Cab {
 				localOrderAdded <- true
-				lamps.SetAllLamps(localelev_ID, all_elevators)
+				lamps.SetAllLamps(localElevID, allElevators)
 			}
 		case <- clearFloor:
-			setOrderList(orders.ClearAtCurrentFloor(all_elevators[localelev_ID]), localelev_ID)
-			lamps.SetAllLamps(localelev_ID, all_elevators)
+			setOrderList(orders.ClearAtCurrentFloor(allElevators[localElevID]), localElevID)
+			lamps.SetAllLamps(localElevID, allElevators)
 
 		case received := <- elevRx:
-			if received.ElevatorID == localelev_ID {
+			if received.ElevatorID == localElevID {
 				break
 			}
 			if received.ElevatorID == "NOTFOUND" {
 				break
 			}
 
-			if _, exists := all_elevators[received.ElevatorID] ; !exists {
-				all_elevators[received.ElevatorID] = unwrapElevator(received)
+			if _, exists := allElevators[received.ElevatorID] ; !exists {
+				allElevators[received.ElevatorID] = unwrapElevator(received)
 				setOperational(true, received.ElevatorID)
 				setConnected(true, received.ElevatorID)
 				fmt.Printf("\nAdded new elevator!\n")
@@ -99,13 +100,13 @@ func UpdateElevator(
 			}
 
 			//Update orders
-			orderMap, isNewLocalOrder, shouldLight := orders.MergeOrders(localelev_ID, getOrderMap(all_elevators), received.Orders)
+			orderMap, isNewLocalOrder, shouldLight := orders.MergeOrders(localElevID, getOrderMap(allElevators), received.Orders)
 			setFromOrderMap(orderMap)
 			if(isNewLocalOrder){
 				localOrderAdded <- true 
 			}
 			if(shouldLight){
-				lamps.SetAllLamps(localelev_ID, all_elevators)
+				lamps.SetAllLamps(localElevID, allElevators)
 			}
 
 		case update := <-connectionUpdate:
@@ -113,42 +114,42 @@ func UpdateElevator(
 			if !update.IsConnected {
 				fmt.Printf("\n%v has been disconnected\n", update.ElevatorID)
 
-				elev, isNewLocalOrder := orders.OrderReassigner(update.ElevatorID, localelev_ID, all_elevators)
-				setOrderList(elev.Orders, localelev_ID)
+				elev, isNewLocalOrder := orders.OrderReassigner(update.ElevatorID, localElevID, allElevators)
+				setOrderList(elev.Orders, localElevID)
 				if isNewLocalOrder {
 					localOrderAdded <- true
 				}
-				lamps.SetAllLamps(localelev_ID, all_elevators)
+				lamps.SetAllLamps(localElevID, allElevators)
 			}
 
 		case update := <-operationUpdate:
 			setOperational(update.IsOperational, update.ElevatorID)
 
-			if !update.IsOperational && update.ElevatorID != localelev_ID { 
+			if !update.IsOperational && update.ElevatorID != localElevID { 
 				fmt.Printf("\nID: %v has been marked as not operational\n", update.ElevatorID)
-				elev, isNewLocalOrder := orders.OrderReassigner(update.ElevatorID, localelev_ID, all_elevators)
-				setOrderList(elev.Orders, localelev_ID)
+				elev, isNewLocalOrder := orders.OrderReassigner(update.ElevatorID, localElevID, allElevators)
+				setOrderList(elev.Orders, localElevID)
 				if isNewLocalOrder {
 					localOrderAdded <- true
 				}
-				lamps.SetAllLamps(localelev_ID, all_elevators)
+				lamps.SetAllLamps(localElevID, allElevators)
 			}
 		case <- tick.C:
 			//transmit local elevator over UDP
-			elevTx <- wrapElevator(localelev_ID)
+			elevTx <- wrapElevator(localElevID)
 		}
 	}
 }
 
 func wrapElevator(elevatorID string) types.WrappedElevator {
 	var wrapped types.WrappedElevator
-	if _, exists := all_elevators[elevatorID] ; exists {
-		temp := all_elevators[elevatorID] 
+	if _, exists := allElevators[elevatorID] ; exists {
+		temp := allElevators[elevatorID] 
 		wrapped.ElevatorID = temp.ElevatorID
 		wrapped.State = temp.State
 		wrapped.Floor = temp.Floor
 		wrapped.Direction = temp.Direction
-		wrapped.Orders = getOrderMap(all_elevators)
+		wrapped.Orders = getOrderMap(allElevators)
 	} else {
 		wrapped.ElevatorID = "NOTFOUND"
 	}
@@ -166,20 +167,11 @@ func unwrapElevator(wrapped types.WrappedElevator) types.Elevator {
 }
 
 func ReadLocalElevator() types.Elevator {
-	return all_elevators[localelev_ID]
+	return allElevators[localElevID]
 }
 
 func ReadAllElevators() map[string]types.Elevator {
-	return all_elevators
-}
-
-func ReadLocalElevatorID() string {
-	return localelev_ID
-}
-
-func keyExists(key string) bool {
-	_, exists := all_elevators[key]
-	return exists
+	return allElevators
 }
 
 func getOrderMap(elevatorMap map[string]types.Elevator) map[string][constants.N_FLOORS][constants.N_BUTTONS]types.Order {
@@ -198,10 +190,10 @@ func setFromOrderMap(orderMap map[string][constants.N_FLOORS][constants.N_BUTTON
 
 /*Workaround functions because go does not allow setting structs in maps directly*/
 func setID(ID string) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		temp.ElevatorID = ID
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	}
 }
 
@@ -212,61 +204,61 @@ func setFields(s types.ElevatorState, f int, d elevio.MotorDirection, ID string)
 }
 
 func setState(s types.ElevatorState, ID string) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		temp.State = s
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	} 
 }
 
 func setFloor(f int, ID string) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		temp.Floor = f
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	}
 }
 
 func setDir(d elevio.MotorDirection, ID string) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		temp.Direction = d
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	}
 }
 
 func setOperational(val bool, ID string) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		temp.IsOperational = val
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	}
 }
 
 func setConnected(val bool, ID string) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		temp.IsConnected = val
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	}
 }
 
 func setOrdered(floor int, button int, ID string, accepted bool) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		if accepted {
 			temp.Orders[floor][button].State = types.OS_AcceptedOrder
 		} else {
 			temp.Orders[floor][button].State = types.OS_UnacceptedOrder
 		}
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	}
 }
 
 func setOrderList(list [constants.N_FLOORS][constants.N_BUTTONS]types.Order, ID string) {
-	temp, is := all_elevators[ID]
+	temp, is := allElevators[ID]
 	if is {
 		temp.Orders = list
-		all_elevators[ID] = temp
+		allElevators[ID] = temp
 	}
 }
